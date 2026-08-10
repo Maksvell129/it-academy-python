3c7f5f850382bc7a6105454be9a69514f7b7c37c
Maksim <93393079+Maksvell129@users.noreply.github.com>
2026-08-10 19:51:45 +0300

    Lesson13 (#2)
    * added main file
    
    * some changes

---
+++ lesson13/main.py
@@ -0,0 +1,25 @@
+def info(name="Aleks", age=10, city="Brest"):
+    print(f"{name=}")
+    print(f"{age=}")
+    print(f"{city=}")
+
+#
+# info("Анна", 25, city="Минск")
+#
+# info(name="Oleg", age=23)
+
+info()
+
+
+def notification(message = "Успешная операция!"):
+    print(f"{message=}")
+
+
+notification()
+notification()
+notification(message="Ошибка")
+notification()
+notification()
+notification()
+notification()
+notification()
